70d79bfa4327b6e4c4f79f65e86731fc8d923211
Ivan Rogachev <rogachevvanya65@gmail.com>
2025-09-02 22:35:47 +0300

    update3

--- src/InMemoryHistoryManager.java
+++ src/InMemoryHistoryManager.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private static final int MAX_HISTORY_SIZE = 10;
-    private final LinkedList<Task> history = new LinkedList<>();
+    private final List<Task> history = new LinkedList<>();
 
     @Override
     public void add(Task task) {
         if (task == null) return;
         if (history.size() >= MAX_HISTORY_SIZE) {
-            history.removeFirst();
+            history.remove(0);
         }
-        history.addLast(task);
+        history.add(task);
     }
 
     @Override
